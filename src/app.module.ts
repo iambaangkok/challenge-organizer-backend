39ca059a189d9efccc47625a38a61a548477da1e
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-01-13 10:35:06 +0700

    added environment variable / config, changed port to 3030

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
@@ -7,9 +8,16 @@ import { challengeSchema } from './modules/Challenge';
 import { UserSchema } from './modules/User';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost:2717/"),
-  MongooseModule.forFeature([{ name: 'User', schema: UserSchema },
-  { name: 'Challenge', schema: challengeSchema }])],
+  imports: [ 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot("mongodb://localhost:2717/"),
+    MongooseModule.forFeature([
+        { name: 'User', schema: UserSchema },
+        { name: 'Challenge', schema: challengeSchema }
+      ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
