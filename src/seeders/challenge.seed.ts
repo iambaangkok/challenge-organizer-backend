26984915753b597730b74d7ecb4133f4c95fa72e
Nonthawt20 <Nonthawt20@gmail.com>
2023-01-15 14:56:42 +0700

    seeder

---
+++ src/seeders/challenge.seed.ts
@@ -0,0 +1,148 @@
+
+const challenge_json = [
+    {
+        "challenge_id": { "$oid": "66b90bf7b8d00f9aa7e7f18a" },
+        "title": "เช้านี้คุณรู้หรือป่าว PM 2.5 เท่าไหร่",
+        "description": "คิดถึงฉันทำไม ไปบอกอีกคนของเธอไป เธอมันใจร้ายคนหลายใจ แต่ลึกเเล้วข้างใน ต้องเก็บเอาไว้ อยู่คนเดียว อยู่ลำพังฉันนอนคนเดียวสบายจะตาย เตียงใหญ่ๆ มีหมอนผ้าห่มฉันนอนคนเดียวสบาย"
+            + "ไม่มีคนมาบ่น ไม่มีคนมาชอบ"
+            + "ไม่ต้องคอยมาง้อมางอน"
+            + "มาบอกฝันดีตอนฉันจะนอน"
+            + "บอกรักฉันทำไม ไปบอกอีกคนของเธอไป"
+            + "เธอมันใจร้ายคนหลายใจ"
+            + "ถ้าเกิดคิดไปไกล เกินไปกว่านี้"
+            + "กลัวว่าใจจะช้ำเอา"
+            + "รู้งี้ ไม่น่าเอาใจไปให้เธอเลย"
+            + "ไม่น่าไปหลงรักใครบางคน"
+            + "ฉันดันไปหลงใบหน้างามๆ"
+            + "ฉันมันใจง่ายชะมัดเลย"
+            + "คิดถึงฉันทำไม ไปบอกอีกคนของเธอไป"
+            + "เธอมันใจร้ายคนหลายใจ"
+            + "ถ้าเกิดคิดไปไกล เกินไปกว่านี้"
+            + "ก็กลัวว่าใจจะช้ำเอง",
+        "participants": [""],
+        "numParticipants": 10,
+        "host": "Object",
+        "img": "String",
+        "max_user": 10,
+        "banUser": [],
+        "publishedStatus": true,
+        "timeStame": "Date",
+        "start": "Date",
+        "end": "Date",
+        "file": {
+            "user": "",
+            "path": "imgfile"
+        },
+        "rewards": [
+            {
+                rankMin: 1,
+                rankMax: 100000,
+                rewardAbsolute: 10,
+            }
+        ],
+        "teams": {
+            "id": 5,
+            "menubar": ["Object"]
+        },
+        "teamMaximum": 10,
+        "feedback": {
+            "date": "",
+            "text": "String",
+            "name": "String"
+        },
+        "rating": {
+            "rating": 3,
+            "total": 10,
+            "coint_ClinkRating": 10
+        },
+        "participantsGiveScore": {
+            "user_Id": "Object",
+            "score": 10,
+        },
+        "schema_version": "String"
+    },
+    {
+        "challenge_id": { "$oid": "5f24e9a9b8e1f9c5e7f87c87" },
+        "title": "Build a Chatbot",
+        "description": "Create a chatbot that can answer questions about a specific topic.",
+        "participants": [
+            {
+                "name": "John Doe",
+                "email": "johndoe@example.com"
+            },
+            {
+                "name": "Jane Smith",
+                "email": "janesmith@example.com"
+            }
+        ],
+        "numParticipants": 2,
+        "host": {
+            "name": "Acme Inc.",
+            "website": "https://www.acme.com"
+        },
+        "ing": "In Progress",
+        "max_user": 5,
+        "banUser": [],
+        "publishedStatus": true,
+        "timeStame": "2022-01-01T00:00:00.000Z",
+        "start": "2022-01-01T00:00:00.000Z",
+        "end": "2022-01-31T23:59:59.999Z",
+        "file": {
+            "user": {
+                "name": "John Doe",
+                "email": "johndoe@example.com"
+            },
+            "path": "chatbot.zip"
+        },
+        "rewards": [
+            {
+                "rankMin": 1,
+                "rankMax": 1,
+                "rewardAbsolute": 1000
+            },
+            {
+                "rankMin": 2,
+                "rankMax": 2,
+                "rewardAbsolute": 500
+            },
+            {
+                "rankMin": 3,
+                "rankMax": 3,
+                "rewardAbsolute": 250
+            }
+        ],
+        "teams": {
+            "id": 1,
+            "menubar": [
+                {
+                    "name": "John Doe",
+                    "email": "johndoe@example.com"
+                },
+                {
+                    "name": "Jane Smith",
+                    "email": "janesmith@example.com"
+                }
+            ]
+        },
+        "teamMaximum": 3,
+        "feedback": {
+            "date": "2022-02-01T00:00:00.000Z",
+            "text": "Great job on the chatbot!",
+            "name": "Acme Inc."
+        },
+        "rating": {
+            "rating": 4.5,
+            "total": 10,
+            "coint_ClinkRating": 0
+        },
+        "participantsGiveScore": {
+            "user_Id": {
+                "name": "John Doe",
+                "email": "johndoe@example.com"
+            },
+            "score": 5
+        },
+        "schema_version": "1.0"
+    }
+
+]
