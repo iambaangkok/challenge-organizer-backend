c58e4b8ebb9dd4ea572ed3fa6df7c88222408b1c
Nonthawt20 <Nonthawt20@gmail.com>
2023-01-15 14:53:50 +0700

    Modules Challenge

--- src/modules/Challenge.ts
+++ src/modules/Challenge.ts
@@ -1,6 +1,72 @@
 import * as mongoose from "mongoose";
+// const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//การ ref ส่ง User
+const challengeSchema_demo = new Schema({
+    // title: String,
+    // body: String,
+    // // username: String,
+    // userid: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
+    // datePosted: {
+    //     type: Date,
+    //     default: new Date()
+    // },
+    // image: String
+    challenge_id: mongoose.Schema.Types.ObjectId,
+    title: String,
+    description: String,
+    participants: [Object],
+    numParticipants: Number,
+    host: Object,
+    img: String,
+    max_user: Number,
+    banUser: [Object],
+    publishedStatus: Boolean,
+    timeStame: new Date(Date.now()),
+    start: Date,
+    end: Date,
+    file: {
+        user: Object,
+        path: String
+    },
+    rewards: [
+        {
+            rankMin: Number,
+            rankMax: Number,
+            rewardAbsolute: Number,
+        }
+    ],
+    teams: {
+        id: Number,
+        menubar: [Object]
+    },
+    teamMaximum: Number,
+    feedback: {
+        date: new Date(Date.now()),
+        text: String,
+        name: String
+    },
+    rating: {
+        rating: Number,
+        total: Number,
+        coint_ClinkRating: Number
+    },
+    participantsGiveScore: {
+        user_Id: Object,
+        score: Number,
+    },
+    schema_version: String
+})
 
 
+const Challenge = mongoose.model('Challenge', challengeSchema_demo);
+
+module.exports = Challenge;
 
 export const challengeSchema = new mongoose.Schema({
     challenge_id: mongoose.Types.ObjectId,
@@ -47,15 +113,5 @@ export const challengeSchema = new mongoose.Schema({
         score: Number,
     },
     schema_version : String
-
-
-
-
-
-
-
-
-
-
 
 })
