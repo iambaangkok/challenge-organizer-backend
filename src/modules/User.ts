4d1624b70582367e7f46a20c590ff14ec038045c
Nonthawt20 <Nonthawt20@gmail.com>
2023-01-15 14:54:10 +0700

    modules user

--- src/modules/User.ts
+++ src/modules/User.ts
@@ -1,7 +1,10 @@
 import * as mongoose from "mongoose";
-// import { ObjextID } from "mongoose";
 
-export const UserSchema = new mongoose.Schema({
+
+const Schema = mongoose.Schema;
+
+
+const user = new Schema({
     user_id: mongoose.Schema.Types.ObjectId,
     username: String,
     passwordHasd: String,
@@ -12,7 +15,7 @@ export const UserSchema = new mongoose.Schema({
     rating: Number,
     challenge: {
         joinedChallenge: {
-            challenge : Object,
+            challenge: Object,
             task: Array,
         },
         banChallenge: [Object],
@@ -24,18 +27,38 @@ export const UserSchema = new mongoose.Schema({
     equipFrame: String,
     status: Boolean,
     task: String,
-    isAdmin: Boolean
+    isAdmin: Boolean,
+    timeStamp: new Date(Date.now())
+})
 
+const User = mongoose.model('User',user);
 
+module.exports= User;
 
-
-
-
-
-
-
-
-
-
-
-});
+// import { ObjextID } from "mongoose";
+// export const UserSchema = new mongoose.Schema({
+//     user_id: mongoose.Schema.Types.ObjectId,
+//     username: String,
+//     passwordHasd: String,
+//     name: {
+//         fname: String,
+//         lname: String,
+//     },
+//     rating: Number,
+//     challenge: {
+//         joinedChallenge: {
+//             challenge : Object,
+//             task: Array,
+//         },
+//         banChallenge: [Object],
+//         creatdeChallenge: [Object],
+//     },
+//     inventory: Array,
+//     coin: Number,
+//     profileIng: String,
+//     equipFrame: String,
+//     status: Boolean,
+//     task: String,
+//     isAdmin: Boolean,
+//     timeStamp : Date
+// });
