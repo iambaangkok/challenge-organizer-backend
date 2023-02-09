a04ecc1e74661a3485e320bc7541e78335421a09
Autoz <kuro123520@hotmail.com>
2023-02-10 00:15:42 +0700

    add findChallenges()

--- src/challenges/service/challenges.service.ts
+++ src/challenges/service/challenges.service.ts
@@ -1,46 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Challenge } from 'src/typeorm/entities/Challenge';
-import { Repository } from 'typeorm';
+import { MongoRepository, Repository } from 'typeorm';
 import { CreateChallengeParams, EditChallengeParams } from '../utils/type';
 
 @Injectable()
 export class ChallengesService {
 
+    constructor(@InjectRepository(Challenge) private challengeRepository: MongoRepository<Challenge>) { }
 
-    constructor(@InjectRepository(Challenge) private challengeRepository: Repository<Challenge>) { }
-
-
-
-    findeAllChallenges() {
-        return this.challengeRepository.find();
+    async findeAllChallenges() {
+        return await this.challengeRepository.find();
     }
 
-
-
-    createChallenge(challengeDetails : CreateChallengeParams) {
-        const newChallenge = this.challengeRepository.create({...challengeDetails,timestamp: new Date()});
-
-        return this.challengeRepository.save(newChallenge);
+    async findChallenges(challengeTitle: string) {
+        return await this.challengeRepository.findOne({ where: {title: challengeTitle} });
     }
-
-
-
-    editChallenge(challenge_Id: String,challengeDetails : EditChallengeParams) {
-        // const challenge = this.challengeRepository.findOne();
-        // challenge = this.challengeRepository.update(...challengeDetails);
-        // return this.challengeRepository.save(challenge);
-
-    }
-
-
-
-    deleteChallenge(challengeId: JSON) {
-        // const deleteChallenge = this.challengeRepository.findOne(challengeId);
-
-        // return this.challengeRepository.delete(deleteChallenge);
-    }
-
-
-
 }
