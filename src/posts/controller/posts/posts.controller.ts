4e1673598c3e140b9b6bf16cc7231dfc44769c73
Autoz <kuro123520@hotmail.com>
2023-03-09 22:37:15 +0700

    finish post module and post controller

--- src/posts/controller/posts/posts.controller.ts
+++ src/posts/controller/posts/posts.controller.ts
@@ -1,22 +1,54 @@
-// // import { Controller, Get, Post } from '@nestjs/common';
-// // import { PostsService } from 'src/posts/services/posts/posts.service';
+import { 
+    Controller, 
+    Get, 
+    Post,
+    Delete,
+    Param,
+    Put,
+    Body
+} from '@nestjs/common';
+import { CreatePostDto } from 'src/dto/CreatePost.dto';
+import { EditPostDto } from 'src/dto/EditPost.dto';
+import { PostsService } from 'src/posts/services/posts/posts.service';
 
-// @Controller('/api/posts')
-// export class PostsController {
-//     constructor(private postService:PostsService){}
+@Controller('/api/posts')
+export class PostsController {
+    constructor(private postService:PostsService){}
 
+    @Get('/:postId')
+    getPostById(@Param('postId') postId: number){
+        console.log(`GET /postId`);
+        return this.postService.findByPostId(postId);
+    }
 
+    @Get('/:tabName')
+    getPostByTab(@Param('tabName') tabName: string){
+        console.log(`GET /tabName`);
+        return this.postService.findPostByTab(tabName);
+    }
 
-//     @Get()
-//     viewPost(){
-//         return this.postService.findPost();
-//     }
+    @Post()
+    createPost(@Body() postDetails: CreatePostDto){
+        console.log(`/`);
+        return this.postService.createPost(postDetails);
+    }
 
+    @Put('/:postId')
+    editPost(
+        @Param('postId') postId: number,
+        @Body() editPostDto: EditPostDto
+    ){
+        console.log(`PUT /${postId}`);
+        return this.postService.editPose(
+            postId,
+            editPostDto
+        )
+    }
 
-//     @Post()
-//     createPost(){
-//         return this.postService.createPost()
-//     }
+    @Delete('/:postId')
+    deletePost(@Param('postId') postId: number){
+        console.log(`DELETE /${postId}`);
+        return this.postService.deletePost(postId);
+    }
 
-
-// }
+}
