ebbd3bfdf99ab1579cc0dd5d25550779954e2d4d
Autoz <kuro123520@hotmail.com>
2023-03-14 04:49:56 +0700

    added BadRequest case when create post and complete all methods in PostService

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,18 +1,24 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeletePostParams, EditPostParams } from 'src/posts/utils/type';
+import { DeletePostParams, EditPostParams, FindPostParams } from 'src/posts/utils/type';
 import { CreatePostParams } from 'src/posts/utils/type';
 import { User } from '../../typeorm/entities/User';
 import { Post } from '../../typeorm/entities/Post';
 import { Repository } from 'typeorm';
+import { Tab } from '../../typeorm/entities/Tab';
+import { Challenge } from '../../typeorm/entities/Challenge';
+import { ChallengesService } from 'src/challenges/service/challenges.service';
 
 @Injectable()
 export class PostsService {
     constructor(
         @InjectRepository(Post)
         private postRepository: Repository<Post>,
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
+        @InjectRepository(Tab)
+        private tabRepository: Repository<Tab>,
+        private challengeService: ChallengesService,
+        @InjectRepository(Challenge)
+        private challengeRepository: Repository<Challenge>
     ) { }
 
     async findAllPost(){
@@ -29,24 +35,83 @@ export class PostsService {
         return post;
     }
 
-    async findPostByTab(tabName: string){
+    async findPostByTab(tabName: string, challengeTitle: FindPostParams){
+        const challenge = await this.challengeService.findChallenges(challengeTitle.challengeTitle)
+        console.log(challenge)
+        if(challenge){
+            let tab;
+            for(let i = 0; i < challenge.tabs.length; ++i){
+                if(challenge.tabs[i].tabName == tabName){
+                    tab = challenge.tabs[i]
+                }
+            }
+            console.log(tab)
+            return tab.posts;
+        } else {
+            throw new HttpException(
+                'Challenge does not exist',
+                HttpStatus.NOT_FOUND
+            );
+        }
         
     }
 
     async createParentPost(postDetails: CreatePostParams) {
-        const newPost = this.postRepository.create({
-            ...postDetails,
-            upDateAt: new Date(),
-            parent: null
+        if(postDetails.challengeTitle == null ||
+            postDetails.tabName == null)
+        {
+            throw new BadRequestException(
+                'You need to put all required fields to create a post',
+            );
+        }
+
+        const challenge = await this.challengeRepository.findOne({
+            relations: {
+                participants: true,
+                tasks: true,
+                collaborators: true,
+                host : true,
+                tabs: true
+            },
+            where: {challengeTitle: postDetails.challengeTitle}
         });
-        await this.postRepository.save(newPost);
-        return newPost;
+        console.log(challenge.tabs)
+        let tab;
+        for(let i = 0; i < challenge.tabs.length; ++i){
+            if(challenge.tabs[i].tabName == postDetails.tabName){
+                tab = challenge.tabs[i]
+            }
+        }
+
+        if(tab){
+            const newPost = this.postRepository.create({
+                content: postDetails.content,
+                upDateAt: new Date(),
+                parent: null,
+                hasTab: tab,
+                hasChallenge: challenge
+            });
+            await this.postRepository.save(newPost);
+            return newPost;
+        } else {
+            throw new HttpException(
+                'Tab does not exist',
+                HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     async createChildrenPost(
         postId: number,
         postDetails: CreatePostParams
     ){
+        if(postDetails.challengeTitle == null ||
+            postDetails.tabName == null)
+        {
+            throw new BadRequestException(
+                'You need to put all required fields to create a post',
+            );
+        }
         const post = await this.findByPostId(postId);
         console.log(post);
         if(post){
@@ -54,7 +119,9 @@ export class PostsService {
             const newPost = this.postRepository.create({
                 ...postDetails,
                 upDateAt: new Date(),
-                parent: post
+                parent: post,
+                hasTab: post.hasTab,
+                hasChallenge: post.hasChallenge
             })
             postList.push(newPost);
             post.children = postList;
@@ -94,7 +161,36 @@ export class PostsService {
         deletePost: DeletePostParams
         )
     {
+        if(deletePost.challengeTitle == null ||
+            deletePost.tabName == null){
+                throw new BadRequestException(
+                    'You need to put all required fields to delete a post',
+                );
+            }
+        const challenge = await this.challengeRepository.findOne({
+            relations: {
+                participants: true,
+                tasks: true,
+                collaborators: true,
+                host : true,
+                tabs: true
+            },
+            where: {challengeTitle: deletePost.challengeTitle}
+        });
 
+        const tab = await this.tabRepository.findOne({
+            relations: {hasChallenge: true, posts: true},
+            where: {tabName: deletePost.tabName, hasChallenge: challenge}
+        });
+
+        if(tab){
+            await this.postRepository.delete({postId: postId})
+        } else {
+            throw new HttpException(
+                'Tab does not exist',
+                HttpStatus.NOT_FOUND
+            )
+        }
     }
 
 }
