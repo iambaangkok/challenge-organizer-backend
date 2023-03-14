351a7084bb0026352d186e84952d27d47f85b9db
Autoz <kuro123520@hotmail.com>
2023-03-14 15:21:11 +0700

    added userservice to postprovider

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChallengesService } from 'src/challenges/service/challenges.service';
+import { UsersService } from 'src/users/services/users.service';
 import { Challenge } from '../typeorm/entities/Challenge';
 import { Post } from '../typeorm/entities/Post';
 import { Tab } from '../typeorm/entities/Tab';
@@ -12,6 +13,6 @@ import { PostsService } from './services/posts.service';
 @Module({
   imports:[TypeOrmModule.forFeature([Challenge,Post,User,Tab])],
   controllers: [PostsController],
-  providers: [PostsService, ChallengesService]
+  providers: [PostsService, ChallengesService, UsersService]
 })
 export class PostsModule {}
