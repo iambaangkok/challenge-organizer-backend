71ec561fc846e3d41e666b6c86480536d983c8b9
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-03-16 07:14:23 +0700

    added getSubmissionsByTaskId

--- src/task/controller/task.controller.ts
+++ src/task/controller/task.controller.ts
@@ -1,63 +1,60 @@
-import { Body, Controller, Get, ParseArrayPipe, ParseIntPipe, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    ParseArrayPipe,
+    ParseIntPipe,
+    Post,
+} from '@nestjs/common';
 import { Delete, Param, Put } from '@nestjs/common/decorators';
-import { CreateTaskDto, EditTaskDto, FindTaskInUser } from '../../dto/CreateTask.dto';
+import {
+    CreateTaskDto,
+    EditTaskDto,
+    FindTaskInUser,
+} from '../../dto/CreateTask.dto';
 import { TaskService } from '../service/task.service';
 
 @Controller('api/task')
 export class TaskController {
-
-    constructor(private taskService: TaskService) { }
-
-
-
+    constructor(private taskService: TaskService) {}
 
     @Get()
     async viewAllTasks() {
         return this.taskService.findTasks();
     }
 
-
+    @Get('/getByTaskId/:taskId')
+    async getSubmissionByTaskId(@Param('taskId') taskId: number) {
+        return this.taskService.findSubmissionByTaskId(taskId);
+    }
 
     // @Get("")
     // async viewTask(task)
 
-
     @Get('/viewByDisPlayName/:displayName')
-    async viewTaskByDisplayName(
-        @Param('displayName') displayName: string 
-    ){
-        // console.log('Fuck ' + displayName.displayName) 
+    async viewTaskByDisplayName(@Param('displayName') displayName: string) {
+        // console.log('Fuck ' + displayName.displayName)
         return this.taskService.findTaskByDisplayName(displayName);
     }
 
-
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto) {
-        const challeneTitle = createTaskDto.challengeTitle
+        const challeneTitle = createTaskDto.challengeTitle;
         return this.taskService.createTask(createTaskDto, challeneTitle);
     }
 
     @Put('/:taskId')
     editeTask(
-        @Param('taskId',ParseIntPipe) taskId: number,
+        @Param('taskId', ParseIntPipe) taskId: number,
         @Body() editTaskDto: EditTaskDto,
     ) {
         console.log(`PUT /${taskId}`);
-        return this.taskService.editedTask(taskId,editTaskDto);
+        return this.taskService.editedTask(taskId, editTaskDto);
     }
 
     @Delete('/:taskId')
-    deleteTask(
-        @Param('taskId',ParseIntPipe) taskId: number
-    ) {
+    deleteTask(@Param('taskId', ParseIntPipe) taskId: number) {
         console.log(`DELETE /${taskId}`);
         return this.taskService.deleteTask(taskId);
     }
-
-
-
-
-
-
-
 }
